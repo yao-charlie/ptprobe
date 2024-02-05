a6743dfdccee1fe7b1a3361e13c0dd8a74edbbf6
yao-charlie <charlieyao@gmail.com>
2024-02-04 16:52:32 -0800

    re-enable file naming, linux port name fix, comment clean up

--- client/examples/read_to_csv.py
+++ client/examples/read_to_csv.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append('../src/ptprobe')
+import os
+from datetime import datetime
 
 import logging
 import threading
 import time
 import argparse
 import board
-from datetime import datetime
 from sinks import CsvSampleSink
 
 boards = []
@@ -24,58 +25,43 @@ if __name__ == "__main__":
             help='Collection time for sampling (s). Default is 0 (no timeout). The nominal sample rate is 5Hz.')
     parser.add_argument('-p', '--ports', default='/dev/ttyACM0',  nargs='+',
             help='Serial port name(s). Default is /dev/ttyACM0.')
-    # parser.add_argument('-f', '--filename', default='test.csv', help='CSV file for data output')
+    parser.add_argument('-f', '--filename', default='', help='Prefix filename for CSV file data output with extension as specified')
     args = parser.parse_args()
 
     logging.info("Starting demo")
     logging.info(args)
 
-    for index, item in enumerate(args.ports):
-        # print(index)
-        # print(item)
-        logging.info("Creating sink for {}".format(item))
-    # logging.info("Creating sink {}".format(args.filename))
-        sinks.append(CsvSampleSink("{}-{}.csv".format(item, datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss"))))
-    # csv = CsvSampleSink(args.filename)
-        sinks[index].open()
-    # csv.open()
+    (prefix, extension) = os.path.splitext(args.filename)  
 
+    for index, item in enumerate(args.ports):
+        logging.info("Creating sink for {}".format(item))
+        port_label = item.split('/')[-1] 
+        sink_name = '_'.join(port_label)
+        sinks.append(CsvSampleSink("{}-{}-{}.{}}".format(prefix, sink_name, datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss")), extension))
+        sinks[index].open()
 
 
     for index, item in enumerate(args.ports):
         boards.append(board.Controller(port=item, sinks=[sinks[index]]))
-    # pt = board.Controller(port=args.ports, sinks=[csv])
         logging.info("Board IDs: {}".format(boards[index].board_id()))
-    # logging.info("Board ID: {}".format(pt.board_id()))
 
 
     logging.info("Main: creating threads")
 
     for index, item in enumerate(args.ports):
         threads.append(threading.Thread(target=boards[index].collect_samples, args=(args.max_count,)))
-    # x = threading.Thread(target=pt.collect_samples, args=(args.max_count,))
         logging.info("Creating thread for {}".format(item))
-    # logging.info("Main: starting thread")
         threads[index].start()
-    # x.start()
 
 
     stop_collection_lambda = lambda: list(map(lambda board: board.stop_collection(), boards))
-    
-    # timer = threading.Timer(args.timeout, list(map(lambda board:board.stop_collection(), boards)))
     timer = threading.Timer(args.timeout, stop_collection_lambda)
-    # t = threading.Timer(args.timeout, pt.stop_collection)
+
     if args.timeout > 0:
         timer.start()
 
-    # print(threads): [<Thread(Thread-1 (collect_samples), started 28484)>, <Thread(Thread-2 (collect_samples), started 29488)>]
-    # print(sinks): [<sinks.CsvSampleSink object at 0x0000020B6AAE3690>, <sinks.CsvSampleSink object at 0x0000020B696B5D10>]
-    # print(boards): [<board.Controller object at 0x0000020B6AC3CE90>, <board.Controller object at 0x0000020B6AC3D090>]
-    # print(list(map(lambda thread:thread.is_alive(), threads))): [True, True]
-
     heartbeat = 0
     while any(list(map(lambda thread:thread.is_alive(), threads))):
-    # while x.is_alive():
         heartbeat += 1
         logging.info("..{}".format(heartbeat))
         time.sleep(1.)
@@ -88,12 +74,10 @@ if __name__ == "__main__":
     logging.info("Main: calling joins")
     for thread in threads:
         thread.join()
-    # x.join()
         
     logging.info("Main: closing sinks")
     for sink in sinks:
         sink.close()
-    # csv.close()
         
 
     logging.info("Main: done")
