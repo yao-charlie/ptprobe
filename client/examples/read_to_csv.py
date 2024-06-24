34e09160b54c1f14a8715aab466af05f3bcafd09
Charlie Yao <charlie.yao@maps-power.com>
2024-06-24 15:27:05 -0400

    BF: Formatting typos

--- client/examples/read_to_csv.py
+++ client/examples/read_to_csv.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     for index, item in enumerate(args.ports):
         logging.info("Creating sink for {}".format(item))
         port_label = item.split('/')[-1] 
-        sink_name = '_'.join(port_label)
-        sinks.append(CsvSampleSink("{}-{}-{}.{}".format(prefix, sink_name, datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss")), extension))
+        sink_name = ''.join(port_label)
+        sinks.append(CsvSampleSink("{}-{}-{}{}".format(prefix, sink_name, datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss"), extension)))
         sinks[index].open()
 
 
