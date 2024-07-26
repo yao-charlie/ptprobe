3652a4ff630b8a782743ca9896d278f7d28c1759
yao-charlie <charlieyao@gmail.com>
2024-07-26 12:20:42 -0700

    Prog: sqlite3 sink

--- client/src/ptprobe/sinks.py
+++ client/src/ptprobe/sinks.py
@@ -2,6 +2,8 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 
+import sqlite3
+
 class SampleSink:
     """The abstract base class for sinks to record streaming sample data"""
 
@@ -84,5 +86,61 @@ class InfluxDBSampleSink (SampleSink):
         #pt.time(sample[0], WritePrecision.MS)
 
         write_api.write(self.bucket, self.org, pt)
+
+class SQLiteSampleSink (SampleSink):
         
+    def __init__(self, url):
+        self.client = None
+        self.url=url
+        self.board_id = 0
+
+    def create(self, com, date):
+        if self.client is None:
+            raise RuntimeError("No sink initialized for write")
+        cursor = self.client.cursor()
+        # cursor.execute("CREATE TABLE {}_{}(channel, timestamp, active_T, fault_T, temperature, ref_temperature, pressure)".format(com, date))
+        cursor.execute("CREATE TABLE {}_{}(channel, timestamp, fault_T, temperature, ref_temperature, pressure)".format(com, date))
+
+    def open(self):
+        self.client = sqlite3.connect(self.url) #need parameter for DB?
+
+    def close(self):
+        if self.client is not None:
+            self.client.close()
+
+    def write(self, sample, com, date):
+        if self.client is None:
+            raise RuntimeError("No sink initialized for write")
+        
+        #Table exist check?
+        
+        cursor = self.client.cursor()
+
+        # pt = Point("board").tag("board_id", self.board_id)
+        # for ich in range(4):
+        #     pt.field("T{}".format(ich), sample[3][ich])
+        #     pt.field("Tref{}".format(ich), sample[4][ich])
+        #     pt.field("P{}".format(ich), sample[5][ich])
+        #     pt.field("Tfault{}".format(ich), sample[2][ich])
+        # pt.time(datetime.utcnow(), WritePrecision.MS)
+
+        #need to parse sample into table format?
+
+        for ich in range(4):
+            cursor.execute("""
+                    INSERT INTO {}_{} VALUES
+                        (Channel{}, {}, {}, {}, {}, {} )
+                """.format(
+                    #Table:
+                    com, 
+                    date, 
+
+                    ich, #channel
+                    datetime.utcnow(), #timestamp
+                    sample[2][ich], #fault_T
+                    sample[3][ich], #temperature
+                    sample[4][ich], #ref_temp
+                    sample[5][ich], #pressure
+                    ))
+
 
