682ba454b2d8dfad34968b690f350cd028eda0b9
yao-charlie <charlieyao@gmail.com>
2024-09-16 13:20:13 -0700

    feat: sync'ed dashboard with arb graphs (channel tested, port not)

--- client/examples/dashboard.py
+++ client/examples/dashboard.py
@@ -2,17 +2,13 @@ import multiprocessing
 import logging
 import argparse
 import multi_read_to_csv
-import platform
 
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html, ALL
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from dash.dependencies import Output, Input
-
-
 
 import os
 import sys
@@ -48,7 +44,8 @@ sensor_data_columns = [ "Time",
                         "CH_0 Ref Temp",	"CH_1 Ref Temp",	"CH_2 Ref Temp",	"CH_3 Ref Temp",
                         "CH_0 Pressure",	"CH_1 Pressure",	"CH_2 Pressure",	"CH_3 Pressure"
                     ]
-sensor_data = pd.read_csv(r"test.csv", header=None, names=sensor_data_columns)
+# sensor_data = pd.read_csv(r"test.csv", header=None, names=sensor_data_columns)
+sensor_data = {}
 
 #note that sensor data from file is actually delayed a few seconds. It is not instantaneous.
 
@@ -60,6 +57,7 @@ external_stylesheets = [
         "rel": "stylesheet",
     },
 ]
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Prototype Sensors"
 
@@ -67,103 +65,156 @@ app.title = "Prototype Sensors"
 
 
 def serve_layout():
+   
+
     return(
-    html.Div(
-        children=[
-            dcc.Interval(
-                id='interval',
-                interval=500, # in milliseconds
-                n_intervals=0
-            ),
-            html.Div(
-                children=[
-                    html.H1(
-                        children="Prototype Sensors", className="header-title"
-                    ),
-                ],
-                className="header",
-            ),
-                  
-            html.Div(
-                children=[
-                    html.Div([
-                        html.P(
-                            children="Motor RPM",
-                            className="menu-title"
+        html.Div(
+            children=[
+                dcc.Interval(
+                    id='interval',
+                    interval=1000, # in milliseconds
+                    n_intervals=0
+                ),
+                html.Div(
+                    children=[
+                        html.H1(
+                            children="Prototype Sensors", className="header-title"
                         ),
-                        dcc.Graph(
-                            id="indicator-motor-RPM",                           
-                        ),
-                        ],
-                        className="card",
-                    ),
-                ],
-                className="wrapper",
-            ),
-        ]
+                    ],
+                    className="header",
+                ),
+                html.Div(
+                    children=[],
+                    # children=[
+                    #     html.Div([
+                    #         html.P(
+                    #             children="Motor RPM",
+                    #             className="menu-title"
+                    #         ),
+                    #         dcc.Graph(
+                    #             id="arb_gauge",                           
+                    #         ),
+                    #         ],
+                    #         className="card",
+                    #     ),
+                    # ],
+                    id="gauge_list",
+                    className="wrapper",
+
+                ),
+            ]
+        )
     )
-    )
+
+
+
+    # gauge_list = []
+    # for item in args.ports:
+    #     gauge_list.append(
+    #     html.Div(
+    #                 children=[
+    #                     html.Div([
+    #                         html.P(
+    #                             children=item,
+    #                             className="menu-title"
+    #                         ),
+    #                         dcc.Graph(
+    #                             id=item,                           
+    #                         ),
+    #                         ],
+    #                         className="card",
+    #                     ),
+    #                 ],
+    #                 className="wrapper",
+    #             ),)
 
 
 app.layout = serve_layout
 
 @app.callback(
     [
-        Output("indicator-motor-RPM", "figure"),
+        Output("gauge_list", "children"),
         ],
-
-    # [Input("interval-temp1", "n_intervals"),
     Input("interval", "n_intervals")
 )
-
 def update_intervals(interval):
     
-    # sensor_data = pd.read_csv(r"./test.csv", header=None,  names=sensor_data_columns)
-
-    # sensor_data = consumer(queue)
     # print(queue.qsize())
-    sensor_data = dictparser(queue.get())["COM5"]
+    for item in args.ports:
+        sensor_data[item] = dictparser(queueList[item].get())[item]
     # sensor_data = dictparser(queueList[].get())["COM5"]
     # print("sensor data:")
-    # print(sensor_data)
+        print(sensor_data[item])
     # print('end data')
 
 
-    indicator_motor_RPM_figure = go.Figure()
+    # test = []
+
+    # for i in "wtf":
+    #     test.append(f"number {i}")
+
+    arb_gauge_figure = go.Figure()
     
-    indicator_motor_RPM_figure.add_trace(go.Indicator(
-        # value = data.loc[0],
-        value = sensor_data[3][0],
-        # value = sensor_data['CH_0 Temp'].iloc[-2],
-        # value = sensor_data.iloc[[-2]][['CH_0 Temp']],
+    arb_gauge_figure.add_trace(go.Indicator(
+        value = sensor_data[item][3][0],
         # delta = {'reference': 160},
         gauge = {'axis': {'visible': True}},
         # gauge_axis_dtick=10, 
         domain = {'x': [0, 1], 'y': [0, 1]},
         # title = {'text': index},
-        mode = "gauge+number"))
+        mode = "gauge+number"
+        ))
+
+    arb_gauge_figure_list = []
+
+    temperature_set = 3
+    channel = 0
+
+    for item in args.ports:
+        for channel in range(4):
+            index_guage_figure = go.Figure(go.Indicator(
+                value = sensor_data[item][temperature_set][channel],
+                gauge = {'axis': {'visible': True}},
+                domain = {'x': [0, 1], 'y': [0, 1]},
+                mode = "gauge+number"
+            ))
+            arb_gauge_figure_list.append(dcc.Graph(figure=index_guage_figure))
+
+    return [arb_gauge_figure_list]
+
+    # return html.Div(
+    #     # [arb_gauge_figure]
+    #     # [html.Div(f'{item}') for item in args.ports]
+    #     [test]
+    #     )
+    # print(arb_gauge_figure_list)
 
 
-    # print(sensor_data.iloc[[-2]][['CH_0 Temp']])
-    # print(sensor_data)
-    return [indicator_motor_RPM_figure]
 
+    # return html.Div(
+    #             [((
+    #                 html.P(
+    #                     children=item,
+    #                     className="menu-title"
+    #                 ),
+    #                 dcc.Graph(
+    #                     id=item,                           
+    #                 )
+    #             ) for item in args.ports)],
 
-# def consumer(queue):
-#     print('Consumer: Running', flush=True)
-#     # consume work
-#     while True:
-#         # get a unit of work
-#         item = queue.get()
-#         # check for stop
-#         if item is None:
-#             break
-#         # report
-#         # print(f'>got {item}', flush=True)
-#         # print(dictparser(item)["COM5"])
-#     # all done
-#         # return item
-#     print('Consumer: Done', flush=True)
+    #         className="card",
+    #         )
+
+# @app.callback(
+#     [
+#         Output("gauge_list", "figure"),
+#         ],
+#     Input("interval", "n_intervals")
+# )
+# def update_graphs(interval):
+#     for item in args.ports:
+#         sensor_data[item] = dictparser(queue)
+
 
 if __name__ == '__main__':
 
@@ -186,25 +237,29 @@ if __name__ == '__main__':
     logging.info(args)
 
     queueList = {}
+    process = {}
 
     for item in args.ports:
         queueList[item] = multiprocessing.Queue()
 
-    print(queueList)
-
     # with multiprocessing.Manager() as manager:
 
-    dataWriter = multi_read_to_csv.readTo(args.filename, args.ports)
-    
+    for item in args.ports:
+        dataWriter = multi_read_to_csv.readTo(args.filename, [item])
+        process[item] = multiprocessing.Process(target = dataWriter.readToCSV, args = (args.max_count, args.timeout, queueList[item]))
+        process[item].start()
 
-    p1 = multiprocessing.Process( target= dataWriter.readToCSV, args = (args.max_count, args.timeout, queue))
+    # p1 = multiprocessing.Process( target= dataWriter.readToCSV, args = (args.max_count, args.timeout, queue))
     # p2 = dashboard
-    p1.start()
+    # p1.start()
 
     # p_consumer = multiprocessing.Process(target = consumer, args = (queue,))
     # p_consumer.start()
     app.run_server(debug=True)
 
-    p1.join()
+    for item in args.ports:
+        process[item].join()
+
+    # p1.join()
     # p_consumer.join()
 
